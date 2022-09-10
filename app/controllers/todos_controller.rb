5c0d5b947e1efae7cc1eebb27f5305b8f3a22344
Katie Bridson <kbridson@memphis.edu>
2022-09-09 20:26:10 -0500

    Fixed indent in controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,13 +1,13 @@
 class TodosController < ApplicationController
+  
+  def index
+    @todos = Todo.order(:due_date)
+    render :index
+  end
 
-    def index
-        @todos = Todo.order(:due_date)
-        render :index
-    end
-
-    def show
-        @todo = Todo.find(params[:id])
-        render :show
-    end
-
+  def show
+    @todo = Todo.find(params[:id])
+    render :show
+  end
+  
 end
