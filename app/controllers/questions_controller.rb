30ab0397b1f4c4341a675d42fc8f9f69c36694de
Katie Bridson <kbridson@memphis.edu>
2022-09-27 22:06:32 -0500

    Updated branch to match new-projects demo, linted quiz views, and updated turbo methods

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,59 +1,59 @@
 class QuestionsController < ApplicationController
+  def index
+    @quiz = Quiz.find(params[:quiz_id])
+    @questions = @quiz.questions
+    render :index
+  end
 
-    def index
-        @quiz = Quiz.find(params[:quiz_id])
-        @questions = @quiz.questions
-        render :index
-    end
+  def show
+    @quiz = Quiz.find(params[:quiz_id])
+    @question = @quiz.questions.find(params[:id])
+    render :show
+  end
 
-    def show
-        @quiz = Quiz.find(params[:quiz_id])
-        @question = @quiz.questions.find(params[:id])
-        render :show
-    end
+  def new
+    @quiz = Quiz.find(params[:quiz_id])
+    @question = Question.new
+    render :new
+  end
 
-    def new
-        @quiz = Quiz.find(params[:quiz_id])
-        @question = Question.new
-        render :new
-      end
-    
-    def create
-        @quiz = Quiz.find(params[:quiz_id])
-        @question = @quiz.questions.build(params.require(:question).permit(:question, :answer, :distractor_1, :distractor_2))
-        if @question.save
-            flash[:success] = "Question saved successfully"
-            redirect_to quiz_questions_url(@quiz)
-        else
-            flash.now[:error] = "Question could not be saved"
-            render :new, status: :unprocessable_entity
-        end
+  def create
+    @quiz = Quiz.find(params[:quiz_id])
+    @question = @quiz.questions.build(
+      params.require(:question).permit(:question, :answer, :distractor_1, :distractor_2)
+    )
+    if @question.save
+      flash[:success] = 'Question saved successfully'
+      redirect_to quiz_questions_url(@quiz)
+    else
+      flash.now[:error] = 'Question could not be saved'
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def edit
-        @quiz = Quiz.find(params[:quiz_id])
-        @question = @quiz.questions.find(params[:id])
-        render :edit
-    end
-    
-    def update
-        @quiz = Quiz.find(params[:quiz_id])
-        @question = @quiz.questions.find(params[:id])
-        if @question.update(params.require(:question).permit(:question, :answer, :distractor_1, :distractor_2))
-            flash[:success] = "Question updated successfully"
-            redirect_to quiz_question_url(@quiz, @question)
-        else
-            flash.now[:error] = "Question could not be updated"
-            render :edit, status: :unprocessable_entity
-        end
-    end
+  def edit
+    @quiz = Quiz.find(params[:quiz_id])
+    @question = @quiz.questions.find(params[:id])
+    render :edit
+  end
 
-    def destroy
-        @quiz = Quiz.find(params[:quiz_id])
-        @question = @quiz.questions.find(params[:id])
-        @question.destroy
-        flash[:success] = "Question deleted successfully"
-        redirect_to quiz_questions_url(@quiz)
+  def update
+    @quiz = Quiz.find(params[:quiz_id])
+    @question = @quiz.questions.find(params[:id])
+    if @question.update(params.require(:question).permit(:question, :answer, :distractor_1, :distractor_2))
+      flash[:success] = 'Question updated successfully'
+      redirect_to quiz_question_url(@quiz, @question)
+    else
+      flash.now[:error] = 'Question could not be updated'
+      render :edit, status: :unprocessable_entity
     end
+  end
 
+  def destroy
+    @quiz = Quiz.find(params[:quiz_id])
+    @question = @quiz.questions.find(params[:id])
+    @question.destroy
+    flash[:success] = 'Question deleted successfully'
+    redirect_to quiz_questions_url(@quiz)
+  end
 end
