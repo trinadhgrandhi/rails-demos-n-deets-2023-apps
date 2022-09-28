57c236ccbf6ff9c2622ee4e31f24977897870635
Katie Bridson <kbridson@memphis.edu>
2022-09-27 22:37:27 -0500

    Fixed route order

--- config/routes.rb
+++ config/routes.rb
@@ -3,24 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
   root to: redirect('/quizzes')
 
   get 'quizzes', to: 'quizzes#index', as: 'quizzes'
   post 'quizzes', to: 'quizzes#create'
   get 'quizzes/new', to: 'quizzes#new', as: 'new_quiz'
+  get 'quizzes/:id/edit', to: 'quizzes#edit', as: 'edit_quiz'
   get 'quizzes/:id', to: 'quizzes#show', as: 'quiz'
   patch 'quizzes/:id', to: 'quizzes#update'
   delete 'quizzes/:id', to: 'quizzes#destroy'
-  get 'quizzes/:id/edit', to: 'quizzes#edit', as: 'edit_quiz'
 
   get 'quizzes/:quiz_id/questions', to: 'questions#index', as: 'quiz_questions'
   post 'quizzes/:quiz_id/questions', to: 'questions#create'
   get 'quizzes/:quiz_id/questions/new', to: 'questions#new', as: 'new_quiz_question'
+  get '/quizzes/:quiz_id/questions/:id/edit', to: 'questions#edit', as: 'edit_quiz_question'
   get '/quizzes/:quiz_id/questions/:id', to: 'questions#show', as: 'quiz_question'
   patch '/quizzes/:quiz_id/questions/:id', to: 'questions#update'
   delete '/quizzes/:quiz_id/questions/:id', to: 'questions#destroy'
-  put '/quizzes/:quiz_id/questions/:id', to: 'questions#update'
-  get '/quizzes/:quiz_id/questions/:id/edit', to: 'questions#edit', as: 'edit_quiz_question'
-
 end
